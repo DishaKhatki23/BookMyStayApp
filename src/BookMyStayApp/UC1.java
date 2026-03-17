8df07e3f5866c654021d9d53834b1aa81c258a9a
DishaKhatki23 <DishaKhatki23@gmail.com>
2026-03-17 14:58:06 +0530

    UC1

---
+++ src/BookMyStayApp/UC1.java
@@ -0,0 +1,28 @@
+/**
+ * UseCase1HotelBookingApp
+ *
+ * This class represents the entry point of the Book My Stay Application.
+ * It demonstrates how a Java program starts execution and prints output.
+ *
+ * @author Dweep
+ * @version 1.0
+ */
+
+public class UC1 {
+
+    /**
+     * Main method - Entry point of the application
+     * JVM starts execution from here
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=========================================");
+        System.out.println("      WELCOME TO BOOK MY STAY APP       ");
+        System.out.println("=========================================");
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: 1.0");
+        System.out.println("Status: Running Successfully...");
+        System.out.println("=========================================");
+
+    }
+}
